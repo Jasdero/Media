d68ef1aea9cde8cd4411139e0961d26d4fc66cd2
Jasdero <houssemaine.j@gmail.com>
2016-11-22 14:26:47 +0100

    crud album ok

---
+++ src/MediaBundle/Entity/Album.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace MediaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Album
+ *
+ * @ORM\Table(name="album")
+ * @ORM\Entity(repositoryClass="MediaBundle\Repository\AlbumRepository")
+ */
+class Album
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre_album", type="string", length=255)
+     */
+    private $titreAlbum;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="artist", type="string", length=255)
+     */
+    private $artist;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="genre", type="string", length=255)
+     */
+    private $genre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="support", type="string", length=255)
+     */
+    private $support;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titreAlbum
+     *
+     * @param string $titreAlbum
+     * @return Album
+     */
+    public function setTitreAlbum($titreAlbum)
+    {
+        $this->titreAlbum = $titreAlbum;
+
+        return $this;
+    }
+
+    /**
+     * Get titreAlbum
+     *
+     * @return string 
+     */
+    public function getTitreAlbum()
+    {
+        return $this->titreAlbum;
+    }
+
+    /**
+     * Set artist
+     *
+     * @param string $artist
+     * @return Album
+     */
+    public function setArtist($artist)
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+
+    /**
+     * Get artist
+     *
+     * @return string 
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * Set genre
+     *
+     * @param string $genre
+     * @return Album
+     */
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    /**
+     * Get genre
+     *
+     * @return string 
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * Set support
+     *
+     * @param string $support
+     * @return Album
+     */
+    public function setSupport($support)
+    {
+        $this->support = $support;
+
+        return $this;
+    }
+
+    /**
+     * Get support
+     *
+     * @return string 
+     */
+    public function getSupport()
+    {
+        return $this->support;
+    }
+}
